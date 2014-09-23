8c9164457df3165152c6def0180c40554f41c04f
sweetoise <sweetoise@ymail.com>
2014-09-23 02:02:55 +0100

    redo all

--- app/models/second_semester_library_book.rb
+++ app/models/second_semester_library_book.rb
@@ -1,20 +1,20 @@
 class SecondSemesterLibraryBook < ActiveRecord::Base
   attr_accessible :author, :book_pages, :description, :name, :second_semester_course_id, :image
   
-   has_attached_file :image,
-                    :storage => :dropbox,
-                    :dropbox_credentials => "#{Rails.root}/config/dropbox_config.yml",
-                    :styles => { :medium => "300x300>", :thumb => "80x100>", :small => "40x50>" },
-                    :dropbox_options => {
-                                          :path => proc { |style| "#{style}/#{id}_#{image.original_filename}" }
-                                        }
+   #has_attached_file :image,
+    #                :storage => :dropbox,
+     #               :dropbox_credentials => "#{Rails.root}/config/dropbox_config.yml",
+      #              :styles => { :medium => "300x300>", :thumb => "80x100>", :small => "40x50>" },
+       #             :dropbox_options => {
+        #                                  :path => proc { |style| "#{style}/#{id}_#{image.original_filename}" }
+         #                               }
 
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  
-  
-  
-  #has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "80x100>", :small => "40x50>" }, :default_url => ""
   #validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  
+  
+  
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "80x100>", :small => "40x50>" }, :default_url => ""
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :second_semester_course
   belongs_to :level
