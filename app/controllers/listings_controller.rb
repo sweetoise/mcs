36adc6eea6537c86b5085d1011e063b99282c311
sweetoise <sweetoise@ymail.com>
2014-10-22 11:00:08 +0100

    mobile app

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,6 +2,10 @@ class ListingsController < Devise::RegistrationsController
   before_filter :authenticate_user!
   before_filter :deny_superadmin, :only => [:edit]
   before_filter :check_user_role, :only => [:edit]
+
+  layout :detect_browser
+  
+  
   def index
     @updates = Update.all
   end
@@ -48,6 +52,16 @@ class ListingsController < Devise::RegistrationsController
     if current_user.has_role? :superadmin
       render :layout => 'administrator'
     end
+  end
+
+  MOBILE_BROWSERS = ["android", "ipod", "opera mini", "blackberry", "palm","hiptop","avantgo","plucker", "xiino","blazer","elaine", "windows ce; ppc;", "windows ce; smartphone;","windows ce; iemobile", "up.browser","up.link","mmp","symbian","smartphone", "midp","wap","vodafone","o2","pocket","kindle", "mobile","pda","psp","treo"]
+
+  def detect_browser
+    agent = request.headers["HTTP_USER_AGENT"].downcase
+    MOBILE_BROWSERS.each do |m|
+      return "mobile_application" if agent.match(m)
+    end
+    return "listings"
   end
 
 end
